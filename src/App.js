f22ee19abfaba482947c09c586392b3a02b26f6e
Jack <j.ackwx11@gmail.com>
2023-04-30 17:14:31 +1000

    quote generator complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomQuote(quotes) {
+  return quotes[Math.floor(Math.random() * quotes.length)];
 }
 
+
+function App() {
+  // states
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+  
+  // fetch quotes 
+  async function fetchData() {
+    const response = await fetch('https://type.fit/api/quotes');
+    const data = await response.json();
+    console.log(data[0]);
+    setQuotes(data);
+    setQuote(data[0]);
+  }
+  useEffect(() => {
+    fetchData();
+    }, []);
+  function getNewQuote() {
+    setQuote(getRandomQuote(quotes));
+  }
+  return (
+    <>
+    <h1>
+      Project 3: Quote Generator
+    </h1>
+    <div className='container'>
+    <div className='quoteBox'>
+      <button onClick={getNewQuote}>
+        New Quote
+      </button>
+      <h3>
+        {quote?.text}
+      </h3>
+      <p>
+        {quote?.author}
+      </p>
+    </div>
+    </div>
+    </>
+  );
+}
+// "https://type.fit/api/quotes"
 export default App;
+
+
